67f38fd5be81481b0f5ddfbaa0c9c5bd7c03d82f
Álvaro Zago <600405561@tupy.com.br>
2022-05-26 09:34:30 -0300

    fix.: Adicionado o id no log

--- src/domain/log.ts
+++ src/domain/log.ts
@@ -1,13 +1,18 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import {v4 as uuidv4} from 'uuid';
 
 @Entity()
 export class Log {
+    
+    @PrimaryColumn()
+    id: string;
 
     @Column( {default: null, type: 'bigint'})
     dataAberturaPorta: string;
 
     static novo(): Log {
         const log: Log = new Log();
+        log.id = uuidv4();
         log.dataAberturaPorta = new Date().getTime().toString();
         return log;
     }
